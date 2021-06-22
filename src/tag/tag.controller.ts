f07d615017f343f167dc9c61a8e54a6b89fc6151
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-06-22 10:04:59 -0300

    add tag service

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from "@nestjs/common";
+import { TagService } from "./tag.service";
 
 @Controller('tags')
 export class TagController {
+
+  constructor(private readonly tagservice: TagService) { }
+
   @Get()
   findAll(): String[] {
-    return ['node', 'javascript', 'nestjs'];
+    return this.tagservice.findAll();
   }
 }
