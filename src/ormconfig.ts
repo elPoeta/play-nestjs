8c19f464ca186dd6fc02545ade9703ee74c14158
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-06-22 10:30:34 -0300

    add typeorm config

---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { ConnectionOptions } from "typeorm";
+
+export const ormconfig : ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'elpoeta',
+  password: 'elpoeta',
+  database: 'playnestjs',
+  entities: [
+    __dirname + '/../**/*.entity{.ts,.js}',
+  ],
+  synchronize: true,
+};
