fc2aeddc63b17f6cedede3213a7f93d8ce3a56c5
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-06-23 10:36:22 -0300

    migrate table tags

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ormconfig } from './ormconfig';
+import ormconfig from './ormconfig';
 import { TagModule } from './tag/tag.module';
 
 @Module({
