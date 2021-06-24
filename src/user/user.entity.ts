f1a43a204dc128c60aa7afc7dfa1143b60b5e0e3
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-06-24 09:38:42 -0300

    hash password before store

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import * as bcrypt from 'bcryptjs';
 
 @Entity({ name: "users" })
 export class UserEntity {
@@ -20,5 +21,15 @@ export class UserEntity {
 
   @Column()
   password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    const salt = await this.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+
+  private async genSalt(salt) {
+    return await bcrypt.genSalt(salt);
+  }
 
 }
