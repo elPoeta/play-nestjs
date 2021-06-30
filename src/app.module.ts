bfdeb5475b0d1c4fdcfd6642366ee1e428f6aede
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-06-30 18:50:48 -0300

    create article module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
 import ormconfig from './ormconfig';
 import { TagModule } from './tag/tag.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
