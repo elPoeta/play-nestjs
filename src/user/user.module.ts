5decb1ca7d1f80858a5d5d747e17f66e071ee165
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-07-03 15:46:56 -0300

    create user repository class

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { AuthGurad } from "./guards/auth.guard";
 import { UserController } from "./user.controller";
 import { UserEntity } from "./user.entity";
 import { UserService } from "./user.service";
+import { UserRepository } from "./userRepository.repository";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UserController],
   providers: [UserService, AuthGurad],
   exports: [UserService]
