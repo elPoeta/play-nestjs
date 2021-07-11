525ba688627c1e80c38bbdebedfa684024e72605
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-07-11 15:29:52 -0300

    create article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { User } from "src/user/decorators/user.decorator";
 import { AuthGurad } from "src/user/guards/auth.guard";
+import { UserResponseInterface } from "src/user/types/userResponseInterace.interface";
 import { ArticleService } from "./article.service";
+import { CreateArticleDto } from "./dto/createArticle.dto.";
+import { ArticleResponseInterface } from "./types/articleResponse.interface";
 
 @Controller('articles')
 export class ArticleController {
@@ -8,7 +12,17 @@ export class ArticleController {
   constructor(private readonly articleService: ArticleService) { }
   @Post()
   @UseGuards(AuthGurad)
-  async createArticle() {
-    return await this.articleService.create();
+  @UsePipes(new ValidationPipe())
+  async createArticle(
+    @User() currentUser: UserResponseInterface,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.create(
+      currentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
   }
 }
+
+
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,13 +1,48 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "src/user/user.entity";
+import { UserRepository } from "src/user/user.repository";
+import { ArticleEntity } from "./article.entity";
 import { ArticleRepository } from "./article.repository";
+import { CreateArticleDto } from "./dto/createArticle.dto.";
+import slugify from 'slugify';
+import { ArticleResponseInterface } from "./types/articleResponse.interface";
+import { UserResponseInterface } from "src/user/types/userResponseInterace.interface";
 
 @Injectable()
 export class ArticleService {
 
-  constructor(@InjectRepository(ArticleRepository) private articleRepository: ArticleRepository){}
+  constructor(@InjectRepository(ArticleRepository) private articleRepository: ArticleRepository,
+  @InjectRepository(UserRepository) private userRepository: UserRepository){}
   
-  async create() {
-    return 'Create article From service'
+  async create(
+    currentUser: UserResponseInterface,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = this.getSlug(createArticleDto.title);
+    delete currentUser.user.token;
+    const user = new UserEntity()
+    Object.assign(user, currentUser.user);
+    article.author = user;
+
+    return await this.articleRepository.save(article);
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, { lower: true }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
   }
 }
