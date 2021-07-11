7495e840815dfa3c0d93fd6825a265b91fd24b57
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2021-07-11 16:40:57 -0300

    find articles with query params

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,15 +1,24 @@
-import { Body, Controller, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { User } from "src/user/decorators/user.decorator";
 import { AuthGurad } from "src/user/guards/auth.guard";
 import { UserResponseInterface } from "src/user/types/userResponseInterace.interface";
 import { ArticleService } from "./article.service";
 import { CreateArticleDto } from "./dto/createArticle.dto.";
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
+import { ArticlesResponseInterface } from "./types/ArticlesResponse.interface";
 
 @Controller('articles')
 export class ArticleController {
 
   constructor(private readonly articleService: ArticleService) { }
+
+  @Get()
+  async findAll(
+    @User('id') currentUserId: number,
+    @Query() query: any,
+  ): Promise<ArticlesResponseInterface> {
+    return await this.articleService.findAll(currentUserId, query);
+  }
 
   @Get(':slug')
   async getSingleArticle(
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -8,6 +8,8 @@ import { CreateArticleDto } from "./dto/createArticle.dto.";
 import slugify from 'slugify';
 import { ArticleResponseInterface } from "./types/articleResponse.interface";
 import { UserResponseInterface } from "src/user/types/userResponseInterace.interface";
+import { ArticlesResponseInterface } from "./types/ArticlesResponse.interface";
+import { getRepository } from "typeorm";
 
 @Injectable()
 export class ArticleService {
@@ -15,6 +17,48 @@ export class ArticleService {
   constructor(@InjectRepository(ArticleRepository) private articleRepository: ArticleRepository,
   @InjectRepository(UserRepository) private userRepository: UserRepository){}
   
+  async findAll(
+    currentUserId: number,
+    query: any,
+  ): Promise<ArticlesResponseInterface> {
+    const queryBuilder =  getRepository(ArticleEntity)
+      .createQueryBuilder('articles')
+      .leftJoinAndSelect('articles.author', 'author');
+
+    if (query.tag) {
+      queryBuilder.andWhere('articles.tagList LIKE :tag', {
+        tag: `%${query.tag}`,
+      });
+    }
+
+    if (query.author) {
+      const author = await this.userRepository.findOne({
+        username: query.author,
+      });
+      queryBuilder.andWhere('articles.authorId = :id', {
+        id: author.id,
+      });
+    }
+     
+    queryBuilder.orderBy('articles.createdAt', 'DESC');
+
+    const articlesCount = await queryBuilder.getCount();
+
+    if (query.limit) {
+      queryBuilder.limit(query.limit);
+    }
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset);
+    }
+
+
+    const articles = await queryBuilder.getMany();
+
+
+    return { articles, articlesCount };
+  }
+
   async create(
     currentUser: UserResponseInterface,
     createArticleDto: CreateArticleDto,
